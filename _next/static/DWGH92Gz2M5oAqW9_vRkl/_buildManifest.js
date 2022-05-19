1fe749096db5afe58ac84515ffbc4e41c5c54260
kevinrpb <kevinrpb@users.noreply.github.com>
2022-05-19 10:57:04 +0000

    deploy: 43b3c900d2ce4b17e077096830cf5817e5189883

---
+++ _next/static/DWGH92Gz2M5oAqW9_vRkl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/255-ac4d3eb4d58aa02d.js","static/chunks/pages/index-1c8e2f85bb510a5e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
